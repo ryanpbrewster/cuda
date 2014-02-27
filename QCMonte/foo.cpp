5c465a62df89a4a6708492cf426723a5815b218b
ryanpbrewster <RyanPBrewster@gmail.com>
2014-02-27 17:32:58 -0600

    Started phasing in C++/CUDA files into the QMC code.

---
+++ QCMonte/foo.cpp
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+extern "C" void foo_() {
+    printf("[RPB] my foo() called\n");
+}
--- QCMonte/main.f90
+++ QCMonte/main.f90
@@ -3,23 +3,20 @@ program qc_main
   use qc_input
   use qc_monte
   use nw_vectors
+  use iso_c_binding
 
   implicit none
 
   print *, 'Starting execution'
-  
+
   call qc_start
 
   call qc_input_read
 
   call nw_vectors_read
 
-  print *, 'Done reading inputs'
-  
-  call qc_monte_energy 
+  call foo
 
-  print *, 'Done with MC'
-  
   call qc_input_mem_free
  
   call qc_end
